81af7cb75a0d6b5c6ee23520af81768c73739fd9
Ben Sandofsky <sandofsky@gmail.com>
2009-05-23 02:39:50 -0700

    Take a step back. We're just going to implement fdx to scrippet for now.

--- lib/logline.rb
+++ lib/logline.rb
@@ -1,71 +1,75 @@
 require 'nokogiri'
 module Logline
-  def self.load(data)
-    Script.new(data)
-  end
-  
-  def self.load_file(path)
-    Script.new File.read(path)
-  end
   
   class Script
     attr_reader :doc
+    attr_reader :paragraphs
+
+    def self.from_fdx(path)
+      new File.read(path)
+    end
+
+    def to_scrippet
+      paragraphs.collect {|paragraph| paragraph.to_scrippet }.join("\n")
+    end
+
+    class Paragraph
+      attr_accessor :content
+      
+      def initialize(c)
+        @content = c
+      end
+      
+      def to_s
+        content
+      end
+
+      def to_scrippet
+        content
+      end
+      
+    end
+
+    class Heading < Paragraph
+      def to_scrippet
+        "#{content.upcase}\n"
+      end
+    end
+
+    class Action < Paragraph 
+      def to_scrippet
+        "#{content}\n"
+      end
+    end
+
+    class CharacterName < Paragraph
+      def to_scrippet
+        content.upcase
+      end
+    end
+
+    class Dialogue < Paragraph 
+      def to_scrippet
+        "#{content}\n"
+      end
+    end
+    class Transition < Paragraph 
+      def to_scrippet
+        "#{content}\n"
+      end
+    end
+    class Unknown < Paragraph ; end
+
+    @@klass_map = {'Scene Heading' => Heading, 'Action' => Action, 'Character' => CharacterName, 'Dialogue' => Dialogue, 'Transition' => Transition}
+
     def initialize(data)
       @doc = Nokogiri::XML(data)
-    end
-    
-    def headings
-      @headings ||= doc.css('Paragraph[@Type="Scene Heading"]').collect {|h| Heading.new(h.inner_text) }
-    end
-    
-    alias_method :slugs, :headings
-    
-    def characters
-      @characters ||= doc.css('Paragraph[@Type="Character"] Text').collect {|c| c.inner_text.upcase }.uniq.collect {|c| Character.new(c) }
-    end
-    
-  end
-  
-  class Character
-    attr_reader :name
-    def initialize(name)
-      @name = name
-    end
-    
-    def to_s
-      name
-    end
-    
-  end
-  
-  class Heading
-    attr_reader :surrounding, :location, :time
-
-    def initialize(text)
-      text =~ /-\s*(.*)\s*$/
-      @time = $1.strip.upcase if $1
-      if @time
-        text =~ /(int\.?|ext\.?)\s*(.*)\s*-\s*#{Regexp.escape @time}\s*$/i
-      else
-        text =~ /(int\.?|ext\.?)\s*(.*)\s*$/i
-      end
-      @surrounding = $1.strip.upcase if $1
-      @location = $2.strip.upcase if $2
-    end
-
-    def to_s
-      if time && surrounding && location
-        "#{surrounding} #{location} - #{time}"
-      elsif surrounding && location
-        "#{surrounding} #{location}"
-      elsif location && time
-        "#{location} - #{time}"
-      elsif surrounding && time
-        "#{surrounding} - #{time}"
-      else
-        location || time || surrounding || ""
+      @paragraphs = doc.xpath('/FinalDraft/Content/Paragraph').collect do |paragraph|
+        text = paragraph.css('Text').collect {|t| t.inner_text }.join('')
+        (@@klass_map[paragraph.attributes['Type'].to_s] || Unknown).new(text)
       end
     end
     
   end
+
 end
--- spec/logline_spec.rb
+++ spec/logline_spec.rb
@@ -2,37 +2,38 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Logline do
   before(:each) do
-    @script = Logline.load_file(File.dirname(__FILE__) + '/fixtures/hacker.fdx')
+    @script = Logline::Script.from_fdx(File.dirname(__FILE__) + '/fixtures/hacker.fdx')
   end
   it "should load an fdx file" do
     @script.should be_kind_of(Logline::Script)
   end
 
-  it "should parse scene headings" do
-    @script.headings.size.should == 2
-    @script.headings.should == @script.slugs
-    @script.headings.collect {|h| h.to_s }.should == ['INT. BEDROOM - NIGHT', 'EXT. HOUSE']
-  end
-  describe "headings" do
-    it "should parse scene heading components" do
-      @heading = Logline::Heading.new("\n      \n      Int. Bedroom - night\n    ")
-      @heading.surrounding.should == 'INT.'
-      @heading.time.should == 'NIGHT'
-      @heading.location.should == 'BEDROOM'
-      @heading.to_s.should == 'INT. BEDROOM - NIGHT'
-    end
-    
-    it "should parse components without the time" do
-      @heading = Logline::Heading.new("\n      \n      EXT. House\n    ")
-      @heading.time.should be_nil
-      @heading.surrounding.should == 'EXT.'
-      @heading.location.should == 'HOUSE'
-      @heading.to_s.should == 'EXT. HOUSE'
-    end
-    
+  it "should recognize all paragraphs" do
+    @script.paragraphs.size.should == 10
+    @script.paragraphs[0].should be_kind_of(Logline::Script::Heading)
+    @script.paragraphs[1].should be_kind_of(Logline::Script::Action)
   end
 
-  it "should parse character names" do
-    @script.characters.collect {|c| c.to_s}.should == ['BEN']
+  it "should convert it into a scrippet" do
+    @script.to_scrippet.should == <<-END
+INT. BEDROOM - NIGHT
+
+A computer nerd in his late 20’s, BENJAMIN SANDOFSKY, hacks away at a ruby library for parsing Final Draft FDX files.
+
+BEN
+Eureka!
+
+He pounds return and we
+
+CUT TO:
+
+EXT. HOUSE
+
+The roof collapses in. The hacker emerges from the ruble, coughs.
+
+BEN
+Back to the drawing board.
+END
   end
+
 end
